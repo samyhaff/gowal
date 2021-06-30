5bc9a1afc9ac97accf4899dcf5d2734aabd79dd8
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-06-30 09:29:28 +0200

    store pixels in slice

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"image/png"
 	"log"
 	"os"
@@ -13,10 +14,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	pixels := make([]color.Color, img.Bounds().Max.Y*img.Bounds().Max.X)
+
 	for y := 0; y < img.Bounds().Max.Y; y++ {
 		for x := 0; x < img.Bounds().Max.X; x++ {
 			color := img.At(x, y)
-			fmt.Printf("%v", color)
+			pixels[img.Bounds().Max.X*y+x] = color
+			// fmt.Printf("%v", color)
 		}
 	}
+	fmt.Println(pixels[:img.Bounds().Max.X])
 }
