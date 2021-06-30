d7025dfba678034f4f8f44db425505c78cea930d
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-06-30 11:58:02 +0200

    added utilitary functions

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"image/png"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 )
+
+const COLORS_NUMBER = 16
 
 type Pixel struct {
 	R int
@@ -13,10 +17,36 @@ type Pixel struct {
 	B int
 }
 
+type Cluster struct {
+	centroid Pixel
+	members  []Pixel
+}
+
 func distance(pixel1, pixel2 Pixel) int {
 	return (pixel1.R-pixel2.R)*(pixel1.R-pixel2.R) +
 		(pixel1.G-pixel2.G)*(pixel1.G-pixel2.G) +
 		(pixel1.B-pixel2.B)*(pixel1.B-pixel2.B)
+}
+
+func mean(pixels []Pixel) Pixel {
+	r, g, b := 0, 0, 0
+	for _, pixel := range pixels {
+		r += pixel.R
+		g += pixel.G
+		b += pixel.B
+	}
+	r, g, b = r/len(pixels), g/len(pixels), b/len(pixels)
+	return Pixel{R: r, G: g, B: b}
+}
+
+func initialize(pixels []Pixel) [COLORS_NUMBER]Pixel {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	var centroids [COLORS_NUMBER]Pixel
+	for i := 0; i < COLORS_NUMBER; i++ {
+		centroids[i] = pixels[r.Intn(len(pixels))]
+	}
+	return centroids
 }
 
 func main() {
